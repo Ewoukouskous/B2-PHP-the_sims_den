31f045a43a1891a59607b0e48eaf1c4b6b869940
Nino <nino.lefebvre@ynov.com>
2026-03-03 10:59:30 +0100

    feat(home page): navbar
    Create the navbar on the top of the home page and add the logo of the website

--- views/home.php
+++ views/home.php
@@ -9,5 +9,14 @@
     <div id="background" class="fixed top-0 left-0 w-full h-full bg-cover bg-center">
         <img src="/public/img/bg.png" alt="Background Image" class="w-full h-full object-cover">
     </div>
+
+    <div id="content" class="relative flex justify-center h-screen p-4">
+        <div id="navbar" class="relative w-full h-12 max-w-7xl p-4 bg-white bg-opacity-80 rounded-4xl shadow-[0px_2px_0px_1.5px_rgba(158,158,158,1)]">
+
+            <div id="logo" class="absolute left-4 top-1/2 transform -translate-y-1/2 text-xl font-bold text-gray-800">
+                <img src="/public/img/plumbob.webp" alt="website logo" class="w-8 h-8 object-contain">
+            </div>
+        </div>
+    </div>
 </body>
 </html>
