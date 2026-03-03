58c738abac0742bed469de8dabeb34832fc31427
Nino <nino.lefebvre@ynov.com>
2026-03-03 11:39:35 +0100

    fix(home page): navbar
    Rework the navbar grid system for a better one

--- views/home.php
+++ views/home.php
@@ -13,9 +13,32 @@
     <div id="content" class="relative flex justify-center h-screen p-4">
         <div id="navbar" class="relative w-full h-12 max-w-7xl p-4 bg-white bg-opacity-80 rounded-4xl shadow-[0px_2px_0px_1.5px_rgba(158,158,158,1)]">
 
-            <div id="logo" class="absolute left-4 top-1/2 transform -translate-y-1/2 text-xl font-bold text-gray-800">
-                <img src="/public/img/plumbob.webp" alt="website logo" class="w-8 h-8 object-contain">
+            <div id="navbar-content" class="grid grid-cols-[repeat(7,1fr)] grid-rows-1 gap-2 h-full">
+
+                <div id="left" class="col-span-3 flex items-center gap-2">
+
+                    <div id="logo" class="absolute top-1/2 transform -translate-y-1/2 text-xl font-bold text-gray-800 p-1">
+                        <img src="/public/img/plumbob.webp" alt="website logo" class="w-8 h-8 object-contain">
+                    </div>
+
+<!--                    <div id="searchbar">-->
+<!--                        <form method="post">-->
+<!--                            <input>-->
+<!--                        </form>-->
+<!--                    </div>-->
+
+                </div>
+
+                <div id="middle" class="col-start-4 flex justify-center">
+
+                </div>
+
+                <div id="right" class="col-start-5 col-span-3 flex flex-row-reverse">
+
+                </div>
+
             </div>
+
         </div>
     </div>
 </body>
