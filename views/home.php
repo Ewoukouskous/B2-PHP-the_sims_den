86b177e7f0318e581f47e009677111e6b2ee844c
Nino <nino.lefebvre@ynov.com>
2026-03-03 09:43:00 +0100

    feat(home page): background
    Set up the php page and add the background to it

---
+++ views/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
+    <div id="background" class="fixed top-0 left-0 w-full h-full bg-cover bg-center">
+        <img src="/public/img/bg.png" alt="Background Image" class="w-full h-full object-cover">
+    </div>
+</body>
+</html>
