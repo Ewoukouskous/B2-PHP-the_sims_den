4e4d36a990805dd4f036f11b9253edc5c29b2426
RAmiard <renaud.amiard@ynov.com>
2026-03-06 00:35:19 +0100

    feat(Models): Make the "Achievement" and "UserAchievement" models
    Just make this two Models :
    - Achievement (achievement) :
      - Attributes
      - Constructor
      - Getters/Setters
    
    - UserAchievement :
      - Attributes (the primary key pair and isAchieved bool)
      - Getters
      - Setter (isAchieved)
    This class is for the pivot table that is used to get all achievement for a user.

---
+++ src/Model/Achievement.php
@@ -0,0 +1,25 @@
+﻿<?php
+
+class Achievement {
+    // ATTRIBUTES
+    private ?int $id = null; // Is equal to null by default, because when we create an achievement (to INSERT it after) we don't know his id yet
+    private string $achievementName;
+    private ?string $achievementDesc = null;
+
+    // CONSTRUCTOR
+    public function __construct(string $achievementName, ?string $achievementDesc = null) {
+        $this->achievementName = $achievementName;
+        $this->achievementDesc = $achievementDesc;
+    }
+
+    // GETTERS
+    public function getId(): ?int {return $this->id;}
+    public function getAchievementName(): string {return $this->achievementName;}
+    public function getAchievementDesc(): ?string {return $this->achievementDesc;}
+
+    // SETTERS
+    public function setId(int $id): void {$this->id = $id;}
+    public function setAchievementName(string $achievementName): void {$this->achievementName = $achievementName;}
+    public function setAchievementDesc(?string $achievementDesc): void {$this->achievementDesc = $achievementDesc;}
+
+}
---
+++ src/Model/UserAchievement.php
@@ -0,0 +1,23 @@
+﻿<?php
+
+class UserAchievement {
+    // ATTRIBUTES
+    private int $idUser;
+    private int $idAchievement;
+    private bool $isAchieved = false;
+
+    // CONSTRUCTOR
+    public function __construct(int $idUser, int $idAchievement) {
+        $this->idUser = $idUser;
+        $this->idAchievement = $idAchievement;
+    }
+
+    // GETTERS
+    public function getIdUser(): ?int {return $this->idUser;}
+    public function getIdAchievement(): int {return $this->idAchievement;}
+    public function getIsAchieved(): bool {return $this->isAchieved;}
+
+    // SETTERS
+    public function setIsAchieved(bool $isAchieved) : void  {$this->isAchieved = $isAchieved;}
+
+}
