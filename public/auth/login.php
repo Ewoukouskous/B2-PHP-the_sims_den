94cd5e1b8992d78a7034dbaf6c154cc7f6f71cc7
Renaud <renaud.amiard@ynov.com>
2026-03-09 20:15:44 +0100

    feat(Authentication): Make the PHP script that handle the login in "login.php"
    The order of all the action that does the script :
    1. Check if there is a valid session with a userId (if yes redirect to HomePage)
    2. Check it's a POST request
    3. If yes, get all the required fields and validate them
    If one of the fields is empty, the user is not found or wrong password we put an error message
    4. One the fields are validated, we prepare the PHPSESSID
    5. Once the PHPSESSID is done we redirect to Homepage

---
+++ public/auth/login.php
@@ -0,0 +1,66 @@
+<?php
+
+if (session_status() === PHP_SESSION_NONE) {session_start();}
+
+// SELECT the actual DIR (public/auth), and ask the go up to the root (2 levels)
+$root_path = dirname(__DIR__, 2);
+
+// Check if the user is already login (already have a userId), we redirect him to the index.php
+if (!empty($_SESSION['userId'])) {
+    header('Location: /index.php');
+    exit();
+}
+
+$error_msg = '';
+
+// If the request is a POST we start the login process
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    // DEPENDENCIES
+    require_once $root_path . '/src/Database/DatabaseConnection.php';
+    require_once $root_path . '/src/Model/UserAccount.php';
+    require_once $root_path . '/src/Repository/UserAccountRepository.php';
+    require_once $root_path . '/src/Enum/UserRole.php';
+    require_once $root_path . '/src/Model/ProfilePic.php';
+    require_once $root_path . '/src/Repository/ProfilePicRepository.php';
+
+    $userRepository = new UserAccountRepository();
+
+    // We get the fields values
+    $username = trim($_POST['username']);
+    $password = $_POST['password'];
+
+    // Check if all the field are filled before checking if the user is valid and connect him
+    if ($username === '' || $password === '') {
+        $error_msg = 'ÉCHEC : Assurez vous que tous les champs soient remplis';
+    } else {
+        // Get the UserAccount object from the database
+        $userByUsername = $userRepository->findByUsername($username);
+        $userAccount = $userByUsername !== null ? $userByUsername : $userRepository->findByEmail($username);
+
+        // If the user account is found and the password hash correspond to the given password we create his PHPSESSION
+        if ($userAccount && password_verify($password, $userAccount->getPasswordHash())) {
+            // Initialize the new PHPSESSID
+            session_regenerate_id(true);
+            $_SESSION['userId'] = $userAccount->getId();
+            $_SESSION['userRole'] = $userAccount->getUserRole()->value;
+            $_SESSION['username'] = $userAccount->getUsername();
+
+            // Get the ProfilePic to get the path
+            $profilePicRepository = new ProfilePicRepository();
+            $profilePic = $profilePicRepository->findById($userAccount->getIdProfilePic());
+
+            if ($profilePic) {
+                $_SESSION['profilePicPath'] = $profilePic->getPicturePath();
+            } else {
+                $_SESSION['profilePicPath'] = $profilePicRepository->findById(1)->getPicturePath();
+            }
+
+            // The PHPSESSION creation is done, now we redirect to the homepage
+            header("Location: /index.php");
+            exit();
+        } else {
+            $error_msg = "Nom d'utilisateur / Email ou le mot de passe est incorrect";
+        }
+    }
+}
+?>
