12058b87cb77e19b8882efb856d1e93dec8fba4a
Nino <nino.lefebvre@ynov.com>
2026-03-09 20:45:31 +0100

    feat(pages):
    Rename hme.php into index.php

--- public/includes/header.php
+++ public/includes/header.php
@@ -1,10 +1,9 @@
 <?php
 
 // Valeurs par défaut si besoin de changer en fonction du code qui include le header
-$basePath = $basePath ?? '';
-$showLoginButton = $showLoginButton ?? true;
-$showProfilePic = $showProfilePic ?? false;
-$searchPlaceholder = $searchPlaceholder ?? 'Recherche :';
+$basePath = isset($basePath) ? $basePath : '';
+$showLoginButton = !isset($showLoginButton) || $showLoginButton;
+$searchPlaceholder = isset($searchPlaceholder) ? $searchPlaceholder : 'Recherche :';
 ?>
 
 <div id="navbar" class="relative w-full h-12 max-w-7xl p-4 bg-[#F0EEE9] bg-opacity-80 rounded-4xl shadow-[0px_2px_0px_1.5px_rgba(158,158,158,1)]">
@@ -39,22 +38,12 @@ $searchPlaceholder = $searchPlaceholder ?? 'Recherche :';
 
         <div id="middle" class="col-start-4 flex justify-center">
 
-            <?php if ($showProfilePic): ?>
-                <a href="#">
-                    <img src="<?php echo $basePath; ?>img/temp/profile_pic.webp"
-                         class="absolute w-20 h-20 object-cover transform -top-10 border-2 rounded-full"
-                         style="border-color: #33b842;"
-                         alt="profile picture"
-                         title="profile picture">
-                </a>
-            <?php else: ?>
                 <a href="<?php echo $basePath; ?>home.php">
                     <img src="<?php echo $basePath; ?>img/plumbob.webp"
                          class="absolute w-16 h-16 object-contain transform -top-8 bg-[#2a5885] border-4 border-[#33b842] rounded-full p-2"
                          alt="logo"
                          title="The Sims Den">
                 </a>
-            <?php endif; ?>
 
         </div>
 
