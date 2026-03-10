999a20a51ebad153527b962ef0fdad2fa779c20c
Nino <nino.lefebvre@ynov.com>
2026-03-10 19:01:00 +0100

    refinings(header): update button styles for registration link

--- public/includes/header.php
+++ public/includes/header.php
@@ -72,7 +72,7 @@ $profilePicPath = $_SESSION['profilePicPath'] ?? 'img/profilePics/cyan_plumbob.p
 
                 <a href="<?php echo $basePath; ?>auth/register.php">
                     <button type="button"
-                            class="px-6 py-1 bg-[#33b842] hover:bg-[#2a9636] rounded-4xl shadow-[0px_2px_0px_1.5px_rgba(158,158,158,1)] text-white font-medium outline-none hover:ring-2 hover:ring-white hover:ring-opacity-50 transition duration-200 ease-in-out">
+                            class="px-6 py-1 bg-[#F0EEE9] bg-opacity-80 rounded-4xl shadow-[0px_2px_0px_1.5px_rgba(158,158,158,1)] text-[#3769a9] font-medium outline-none hover:ring-2 hover:ring-[#3769a9] hover:ring-opacity-50 transition duration-200 ease-in-out">
                         S'inscrire
                     </button>
                 </a>
