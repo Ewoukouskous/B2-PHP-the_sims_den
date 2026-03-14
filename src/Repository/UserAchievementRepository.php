c917b2a194e1e26e06c1893c3900239dd192343d
Renaud <renaud.amiard@ynov.com>
2026-03-14 20:38:14 +0100

    fix(Repository): Fixed the imports in the repositories
    I made the mistake of forgetting to import the dependencies in the models Repositories.
    
    Because the web pages that was using them was importing the depencies by themself, that was working because the repositories was inheriting the dependencies from the web pages
    
    Now it load the dependencies by themself

--- src/Repository/UserAchievementRepository.php
+++ src/Repository/UserAchievementRepository.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once __DIR__ . '/../Database/DatabaseConnection.php';
+require_once __DIR__ . '/../Model/UserAchievement.php';
+
 class UserAchievementRepository {
     // ATTRIBUTES
     private PDO $pdo;
