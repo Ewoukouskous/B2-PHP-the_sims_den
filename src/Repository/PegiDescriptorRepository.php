284257e61c6a464e021b92ead9cf62e201ce0d37
Renaud <renaud.amiard@ynov.com>
2026-03-14 20:38:14 +0100

    fix(Repository): Fixed the imports in the repositories
    I made the mistake of forgetting to import the dependencies in the models Repositories.
    
    Because the web pages that was using them was importing the depencies by themself, that was working because the repositories was inheriting the dependencies from the web pages
    
    Now it load the dependencies by themself

--- src/Repository/PegiDescriptorRepository.php
+++ src/Repository/PegiDescriptorRepository.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once __DIR__ . '/../Database/DatabaseConnection.php';
+require_once __DIR__ . '/../Model/PegiDescriptor.php';
+
 class PegiDescriptorRepository {
     // ATTRIBUTES
     private PDO $pdo;
