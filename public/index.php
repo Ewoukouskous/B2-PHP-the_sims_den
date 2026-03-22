ad36c28ac96cefc3e5a729f97b959f67a5337534
Nino <nino.lefebvre@ynov.com>
2026-03-22 08:22:21 +0100

    feat: Modified the fav link to a fav button on the index page to avoid misery while we are not connected
    it was due to a bad <a> balise and i edit it with a <button> one

--- public/index.php
+++ public/index.php
@@ -174,7 +174,8 @@ if ($currentUserId !== null) {
                                                 </button>
                                             <?php endif; ?>
                                         <?php else: ?>
-                                            <a href="auth/login.php"
+                                            <button type="button"
+                                                onclick="event.preventDefault(); event.stopPropagation(); window.location.href='auth/login.php';"
                                                 class="pointer-events-auto bg-[#F0EEE9] p-2 rounded-full shadow-[0px_2px_0px_1.5px_rgba(158,158,158,1)] flex items-center justify-center hover:scale-110 transition-transform cursor-pointer"
                                                 aria-label="Se connecter pour ajouter aux favoris">
                                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-[#3769a9]" fill="none"
@@ -182,7 +183,7 @@ if ($currentUserId !== null) {
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                         d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                                                 </svg>
-                                            </a>
+                                            </button>
                                         <?php endif; ?>
                                         <span
                                             class="absolute -bottom-4 text-[9px] font-bold text-[#3769a9]"><?php echo $game->getFavoritesNumber(); ?></span>
