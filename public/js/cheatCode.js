dab7e57464c2c835e1d360ae2a9eedeeffc959c8
Renaud <renaud.amiard@ynov.com>
2026-03-23 15:29:18 +0100

    refactor(Achievement): Now use the good php script to unlock the Motherlode success
    Show an alert when motherlode is correctly unlocked

--- public/js/cheatCode.js
+++ public/js/cheatCode.js
@@ -16,20 +16,28 @@
 
         // Check if the sequence matches the cheat code
         if (input === cheatCode) {
+            console.log("'motherlode' cheat detected");
+
             // Send the cheat code to the backend via POST request
-            fetch("/actions/cheat.php", {
+            fetch("/actions/unlockMotherlode.php", {
                 method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ cheat: "motherlode" })
             })
-                .then(response => response.json())
-                .then(data => {
-                    // If the achievement is successfully unlocked, reload the page to display it
-                    if (data.success) {
+                .then(async (response) => {
+                    // Get the raw response and clean the BOM invisible character that make crash the JSON parsing
+                    const raw = await response.text();
+                    const cleaned = raw.replace(/^\uFEFF+/, "");
+                    return JSON.parse(cleaned);
+                })
+                // Then we take the json data to check if the request succeed
+                .then((data) => {
+                    if (data.succeed) {
+                        alert("Motherlode débloqué! Jette un oeil à tes succès.");
                         window.location.reload();
+                    } else {
+                        console.log("Error while unlocking 'Motherlode' achievement : " + data.error);
                     }
                 })
-                .catch(error => console.error("Cheat error:", error));
+                .catch((error) => console.error("Cheat error:", error));
 
             // Reset the input string after a successful match
             input = "";
